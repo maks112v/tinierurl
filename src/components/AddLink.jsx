19c94c6968ff8b791f56b4d4770d2e45b04ef484
Maksim V <maks112v@gmail.com>
2019-06-26 14:48:05 -0400

    working on getting data

--- src/components/AddLink.jsx
+++ src/components/AddLink.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { store } from '../config/firebase';
 
+import { authContext } from '../hooks/Auth';
+
 export default function AddLink() {
-  const allowedDomains = [];
+  let selectDomains = [];
+  const user = useContext(authContext);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [longLink, setLongLink] = useState('');
+
+  useEffect(() => {
+    const unsubscribe = store
+      .collection('domains')
+      .where('access', '==', 'all')
+      .onSnapshot(snapShot => {
+        console.log(snapShot);
+        snapShot.docs.map(doc => {
+          selectDomains.push(doc.id);
+        });
+        setIsLoading(false);
+      });
+
+    return () => unsubscribe();
+  });
+
+  console.log(selectDomains);
 
   return (
-    <>
-      <form action="">
-        <input type="text" placeholder="Long Link" />
-        <select />
-      </form>
-    </>
+    <form action="">
+      <input
+        type="text"
+        placeholder="Long Link"
+        value={longLink}
+        onChange={e => setLongLink(e.target.value)}
+        disabled={isLoading}
+      />
+      <select disabled={isLoading} />
+    </form>
   );
 }
