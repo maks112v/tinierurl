18eef49710b4b0f462e26fb26fd834244f77bb78
Maksim V <maks112v@gmail.com>
2019-06-28 11:09:14 -0400

    item error

--- src/components/ShowLinks.jsx
+++ src/components/ShowLinks.jsx
@@ -9,39 +9,37 @@ import { useSession } from '../hooks/Auth';
 export default function ShowLinks() {
   const user = useSession();
 
-  const [docs, setDocs] = useState({});
+  const [docs, setDocs] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    let items = [];
     const unsubscribe = store
       .collection('users')
       .doc(user.uid)
       .collection('redirects')
       .onSnapshot(snapShot => {
-        const items = {};
         snapShot.forEach(getPath => {
           const { path } = getPath.data();
           store.doc(path).onSnapshot(doc => {
             const data = doc.data();
-            items[doc.id] = data;
+            items.push({ ...data });
           });
         });
-        setDocs(items);
-        setIsLoading(false);
       });
-
+    setDocs(items);
+    setIsLoading(false);
     return () => unsubscribe();
   }, []);
 
   console.log(docs);
-
   return (
     <div style={{ margin: '30px 0', backgroundColor: 'white' }}>
       <Table
         pagination={false}
         loading={isLoading}
         columns={[{ title: 'Url', dataIndex: 'url', key: 'url' }]}
-        dataSource={Object.values(docs)}
+        dataSource={docs}
         bordered={true}
         rowKey="id"
       />
