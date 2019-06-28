30f74de40e1b0b61c523157b6853a3bdf3d4ff5b
Maksim V <maks112v@gmail.com>
2019-06-27 23:24:00 -0400

    adding styling and content

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 import PrivateRoute from './components/PrivateRoute';
 import { authContext, useAuth, authHandler } from './hooks/Auth';
+import { Layout, Menu, Spin, Typography } from 'antd';
 
 import Dashboard from './views/Dashboard';
 import Login from './views/Login';
@@ -10,21 +11,65 @@ import ToLink from './views/ToLink';
 
 function App() {
   const { user, initializing } = useAuth();
-  if (initializing) {
+  const [delay, setDelay] = useState(false);
+  setTimeout(() => setDelay(false), 3000);
+  if (initializing || delay) {
     return (
-      <div>
-        <h1>Loading</h1>
-      </div>
+      <Layout style={{ backgroundColor: '#F9FBFD' }}>
+        <Layout.Content
+          style={{
+            minHeight: '100vh',
+            textAlign: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '2rem',
+          }}
+        >
+          <div>
+            <Spin size="large" />
+            <Typography.Title style={{ fontWeight: 'bold' }}>
+              Tinier
+              <span style={{ color: '#2196F3' }}>Url</span>
+            </Typography.Title>
+            <Typography.Title level={4} style={{ color: '#757575' }}>
+              Create and share trusted, powerful short links
+            </Typography.Title>
+          </div>
+        </Layout.Content>
+      </Layout>
     );
   }
   return (
     <authContext.Provider value={user}>
-      <button onClick={authHandler}>Logout</button>
-      <Switch>
-        <Route exact path="/login" render={props => <Login {...props} />} />
-        <PrivateRoute exact path="/" component={Dashboard} />
-        <Route path="/:id" render={props => <ToLink {...props} />} />
-      </Switch>
+      {/* <button onClick={authHandler}>Logout</button> */}
+      <Layout
+        style={{
+          backgroundColor: '#F9FBFD',
+          minHeight: '100vh',
+        }}
+      >
+        {/* {user && (
+          <Layout.Header>
+            <Menu theme="dark" mode="horizontal" style={{ lineHeight: '64px' }}>
+              <Menu.Item onClick={authHandler}>Logout</Menu.Item>
+            </Menu>
+          </Layout.Header>
+        )} */}
+        <Layout.Content>
+          <div style={{ maxWidth: 1200, margin: '20px auto', padding: '1rem' }}>
+            <Switch>
+              <Route
+                exact
+                path="/login"
+                render={props => <Login {...props} />}
+              />
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <Route path="/:id" render={props => <ToLink {...props} />} />
+            </Switch>
+          </div>
+        </Layout.Content>
+      </Layout>
     </authContext.Provider>
   );
 }
--- src/components/AddLink.jsx
+++ src/components/AddLink.jsx
@@ -1,29 +1,45 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { store } from '../config/firebase';
 
+import { Input, Card, Select, Form, Button, message } from 'antd';
+
+import copy from 'copy-to-clipboard';
+
 import { authContext } from '../hooks/Auth';
+
+function validateInput({ value, type }) {
+  if (type === 'url') {
+    return /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(
+      value,
+    )
+      ? 'success'
+      : 'error';
+  }
+}
 
 export default function AddLink() {
   const user = useContext(authContext);
 
   const [isLoading, setIsLoading] = useState(true);
-  const [longLink, setLongLink] = useState('');
+  const [longLink, setLongLink] = useState({ value: '', val: '' });
   const [domain, setDomain] = useState('');
   const [selectDomains, setSelectDomains] = useState([]);
-
   function submitHandler(e) {
     e.preventDefault();
+    setIsLoading(true);
     store
       .collection('domains')
       .doc(domain)
       .collection('redirects')
       .add({
-        url: longLink,
+        url: longLink.value,
         owner: user.uid,
       })
       .then(res => {
-        setLongLink('');
-        setDomain('');
+        setLongLink({ value: '', val: '' });
+        setIsLoading(false);
+        copy(`${selectDomains.find(x => x.id === domain).hostname}/${res.id}`);
+        message.success('Link Copied to Clipboard');
       });
   }
 
@@ -46,27 +62,77 @@ export default function AddLink() {
   }, []);
 
   return (
-    <form onSubmit={submitHandler}>
-      <input
-        type="text"
-        placeholder="Long Link"
-        value={longLink}
-        onChange={e => setLongLink(e.target.value)}
-        disabled={isLoading}
-      />
-      <select
-        disabled={isLoading}
-        value={domain}
-        onChange={e => setDomain(e.target.value)}
-      >
-        <option value="">Select a Domain</option>
-        {selectDomains.map(({ id, hostname }) => (
-          <option key={id} value={id}>
-            {hostname}
-          </option>
-        ))}
-      </select>
-      <button type="submit">Create</button>
-    </form>
+    <Card
+      style={{ margin: '30px 0', backgroundColor: 'white' }}
+      title="Create Link"
+    >
+      <Form onSubmit={submitHandler}>
+        <div style={{ display: 'flex' }}>
+          <Form.Item
+            hasFeedback
+            validateStatus={longLink.val}
+            style={{ marginRight: 10, flexGrow: 1 }}
+          >
+            <Input
+              placeholder="Long Url"
+              value={longLink.value}
+              onChange={e =>
+                setLongLink({
+                  value: e.target.value,
+                  val: validateInput({ value: e.target.value, type: 'url' }),
+                })
+              }
+            />
+          </Form.Item>
+          <Form.Item hasFeedback style={{ width: '25%', marginRight: 10 }}>
+            <Select
+              showSearch
+              onChange={e => setDomain(e)}
+              placeholder="Select a Domain"
+            >
+              {selectDomains.map(({ id, hostname }) => (
+                <Select.Option key={id} value={id}>
+                  {hostname}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+          <Form.Item>
+            <Button
+              disabled={
+                longLink.val !== 'success' || domain === '' || isLoading
+              }
+              type="primary"
+              loading={isLoading}
+              htmlType="submit"
+            >
+              Submit
+            </Button>
+          </Form.Item>
+        </div>
+      </Form>
+    </Card>
+    // <form onSubmit={submitHandler}>
+    //   <input
+    //     type="text"
+    //     placeholder="Long Link"
+    //     value={longLink}
+    //     onChange={e => setLongLink(e.target.value)}
+    //     disabled={isLoading}
+    //   />
+    //   <select
+    //     disabled={isLoading}
+    //     value={domain}
+    //     onChange={e => setDomain(e.target.value)}
+    //   >
+    //     <option value="">Select a Domain</option>
+    //     {selectDomains.map(({ id, hostname }) => (
+    //       <option key={id} value={id}>
+    //         {hostname}
+    //       </option>
+    //     ))}
+    //   </select>
+    //   <button type="submit">Create</button>
+    // </form>
   );
 }
